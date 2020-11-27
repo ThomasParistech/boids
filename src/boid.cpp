3688ca338d26360729d8348a365e4d3d0a55e00b
ThomasR <trouch@prophesee.ai>
2020-11-27 19:37:39 +0100

    use prettier bird

--- src/boid.cpp
+++ src/boid.cpp
@@ -87,23 +87,19 @@ void Boid::update(float dt)
     separation_force_ = Vec3f(0, 0, 0);
 }
 
-
 void Boid::draw() const
 {
-    std::cout << "Draw " << std::endl
-              << "Position: " << position_.transpose() << std::endl
-              << "Speed   : " << speed_.transpose() << std::endl;
     glPushMatrix();
 
     glTranslatef(position_[0], position_[1], position_[2]);
     GlUtils::align_view(speed_);
-    // GlUtils::draw_box(Vec3f(0.6, 0.3, 0.3));
+    GlUtils::draw_box(Vec3f(0.6, 0.3, 0.3));
 
-    // glTranslatef(-0.6, 0, 0);
-    // GlUtils::draw_yz_plane(0.4, 0.4, Vec3f(0.0, 0.0, 1.0));
+    glTranslatef(-0.6, 0, 0);
+    GlUtils::draw_yz_plane(0.4, 0.4, Vec3f(0.0, 0.0, 1.0));
 
-    // glTranslatef(1.2, 0, 0);
-    // GlUtils::draw_pyramid(Vec3f(0.1, 0.1, 0.1), Vec3f(1.0, 1.0, 0.0));
-    GlUtils::draw_pyramid(size_ * Vec3f(1.0, 0.1, 0.1), Vec3f(0.2, 0.3, 0.8));
+    glTranslatef(1.2, 0, 0);
+    GlUtils::draw_pyramid(Vec3f(0.1, 0.1, 0.1), Vec3f(1.0, 1.0, 0.0));
+    //GlUtils::draw_pyramid(size_ * Vec3f(1.0, 0.1, 0.1), Vec3f(0.2, 0.3, 0.8));
     glPopMatrix();
 }
